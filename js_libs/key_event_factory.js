e7c1e0da6884b04f7d45ee3539edbdffc0d454d3
Fischer-L <Fischer-L@users.noreply.github.com>
2015-10-13 17:27:02 +0800

    Update key_event_factory.js

--- js_libs/key_event_factory.js
+++ js_libs/key_event_factory.js
@@ -1,88 +1,109 @@
-'use strict';
+(function(exports) {
+  'use strict';
 
-var keyEventFactory = {};
+  var keyEventFactory = {};
 
-var _KEY_PROP = keyEventFactory._KEY_PROP = {};
+  var _KEY_PROP = keyEventFactory._KEY_PROP = {};
 
-_KEY_PROP.UP = {};
-_KEY_PROP.UP.key =
-_KEY_PROP.UP.code =
-_KEY_PROP.UP.keyIdentifier = 'Up';
-_KEY_PROP.UP.which =
-_KEY_PROP.UP.keyCode = 33;
+  _KEY_PROP.UP = {};
+  _KEY_PROP.UP.key =
+  _KEY_PROP.UP.code =
+  _KEY_PROP.UP.keyIdentifier = 'Up';
+  _KEY_PROP.UP.which =
+  _KEY_PROP.UP.keyCode = 33;
 
-_KEY_PROP.DOWN = {};
-_KEY_PROP.DOWN.key =
-_KEY_PROP.DOWN.code =
-_KEY_PROP.DOWN.keyIdentifier = 'Down';
-_KEY_PROP.DOWN.which =
-_KEY_PROP.DOWN.keyCode = 40;
+  _KEY_PROP.DOWN = {};
+  _KEY_PROP.DOWN.key =
+  _KEY_PROP.DOWN.code =
+  _KEY_PROP.DOWN.keyIdentifier = 'Down';
+  _KEY_PROP.DOWN.which =
+  _KEY_PROP.DOWN.keyCode = 40;
 
-_KEY_PROP.LEFT = {};
-_KEY_PROP.LEFT.key =
-_KEY_PROP.LEFT.code =
-_KEY_PROP.LEFT.keyIdentifier = 'Left';
-_KEY_PROP.LEFT.which =
-_KEY_PROP.LEFT.keyCode = 37;
+  _KEY_PROP.LEFT = {};
+  _KEY_PROP.LEFT.key =
+  _KEY_PROP.LEFT.code =
+  _KEY_PROP.LEFT.keyIdentifier = 'Left';
+  _KEY_PROP.LEFT.which =
+  _KEY_PROP.LEFT.keyCode = 37;
 
-_KEY_PROP.RIGHT = {};
-_KEY_PROP.RIGHT.key =
-_KEY_PROP.RIGHT.code =
-_KEY_PROP.RIGHT.keyIdentifier = 'Right';
-_KEY_PROP.RIGHT.which =
-_KEY_PROP.RIGHT.keyCode = 39;
+  _KEY_PROP.RIGHT = {};
+  _KEY_PROP.RIGHT.key =
+  _KEY_PROP.RIGHT.code =
+  _KEY_PROP.RIGHT.keyIdentifier = 'Right';
+  _KEY_PROP.RIGHT.which =
+  _KEY_PROP.RIGHT.keyCode = 39;
 
-keyEventFactory.KEY = {
-  UP : 'UP',
-  DOWN : 'DOWN',
-  LEFT : 'LEFT',
-  RIGHT : 'RIGHT'
-};
+  _KEY_PROP.ENTER = {};
+  _KEY_PROP.ENTER.key =
+  _KEY_PROP.ENTER.code =
+  _KEY_PROP.ENTER.keyIdentifier = 'Enter';
+  _KEY_PROP.ENTER.which =
+  _KEY_PROP.ENTER.keyCode = 13;
 
-keyEventFactory.EVENT = {
-  KEY_UP : 'KEY_UP',
-  KEY_DOWN : 'KEY_DOWN',
-  KEY_PRESS : 'KEY_PRESS',
-  _map : {
-    KEY_UP : 'keyup', KEY_DOWN : 'keydown', KEY_PRESS : 'keypress'
-  }
-};
+  _KEY_PROP.ENTER = {};
+  _KEY_PROP.ENTER.key =
+  _KEY_PROP.ENTER.code =
+  _KEY_PROP.ENTER.keyIdentifier = 'Esc';
+  _KEY_PROP.ENTER.which =
+  _KEY_PROP.ENTER.keyCode = 27;
 
-/**
- * @param type The event type. See {@link this.EVENT}
- * @param targetKey The key of event. See {@link this.KEY}
- * @return {EVENT} Key event instance
- */
-keyEventFactory.makeEvent = function (type, targetKey) {
+  keyEventFactory.KEY = {
+    UP : 'UP',
+    DOWN : 'DOWN',
+    LEFT : 'LEFT',
+    RIGHT : 'RIGHT',
+    ENTER : 'ENTER',
+    ESC : 'ESC'
+  };
 
-  var k = this.KEY[targetKey],
-      t = this.EVENT._map[this.EVENT[type]];
+  keyEventFactory.EVENT = {
+    KEY_UP : 'KEY_UP',
+    KEY_DOWN : 'KEY_DOWN',
+    KEY_PRESS : 'KEY_PRESS',
+    _map : {
+      KEY_UP : 'keyup', KEY_DOWN : 'keydown', KEY_PRESS : 'keypress'
+    }
+  };
 
-  if (t && k) {
-    var e = new Event(t);
-    e.key = this._KEY_PROP[k].key;
-    e.code = this._KEY_PROP[k].code;
-    e.keyIdentifier = this._KEY_PROP[k].keyIdentifier;
-    e.which = this._KEY_PROP[k].which;
-    e.keyCode = this._KEY_PROP[k].keyCode;
-    return e;
-  }
+  /**
+   * @param type The event type. See {@link this.EVENT}
+   * @param targetKey The key of event. See {@link this.KEY}
+   * @return {EVENT} Key event instance
+   */
+  keyEventFactory.makeEvent = function (type, targetKey) {
 
-  throw new Error(
-    'Fail to make key event due to unknown ' +
-    'type of ' + type + ' or ' +
-    'key of ' + targetKey
-  );
-};
+    var k = this.KEY[targetKey],
+        t = this.EVENT._map[this.EVENT[type]];
 
-keyEventFactory.makeKeyup = function (targetKey) {
-  return this.makeEvent(this.EVENT.KEY_UP, targetKey);
-};
+    if (t && k) {
+      var e = new Event(t);
+      e.key = this._KEY_PROP[k].key;
+      e.code = this._KEY_PROP[k].code;
+      e.keyIdentifier = this._KEY_PROP[k].keyIdentifier;
+      e.which = this._KEY_PROP[k].which;
+      e.keyCode = this._KEY_PROP[k].keyCode;
+      return e;
+    }
 
-keyEventFactory.makeKeydown = function (targetKey) {
-  return this.makeEvent(this.EVENT.KEY_DOWN, targetKey);
-};
+    throw new Error(
+      'Fail to make key event due to unknown ' +
+      'type of ' + type + ' or ' +
+      'key of ' + targetKey
+    );
+  };
 
-keyEventFactory.makeKeypress = function (targetKey) {
-  return this.makeEvent(this.EVENT.KEY_PRESS, targetKey);
-};
+  keyEventFactory.makeKeyup = function (targetKey) {
+    return this.makeEvent(this.EVENT.KEY_UP, targetKey);
+  };
+
+  keyEventFactory.makeKeydown = function (targetKey) {
+    return this.makeEvent(this.EVENT.KEY_DOWN, targetKey);
+  };
+
+  keyEventFactory.makeKeypress = function (targetKey) {
+    return this.makeEvent(this.EVENT.KEY_PRESS, targetKey);
+  };
+
+  exports.mockKeyEvent = keyEventFactory;
+
+})(window);
